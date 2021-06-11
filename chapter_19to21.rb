973bd410ea0d4a9545465d426bac084815809e9d
GustavoAndresRamirezMunoz <gustavo.ramirez@kommit.co>
2021-06-11 17:58:22 -0500

    chapter 19 to 21

---
+++ chapter_19to21.rb
@@ -0,0 +1,40 @@
+p 1 + 2
+p 1 - 2
+p 3 * 4
+p 3 + 8 * 2 -1
+
+p 5 / 2
+p 5.0 / 2
+p 5 / 2.0
+
+p 5 ** 2
+p 2 ** 3
+
+p 12 % 4
+p  5 % 2
+
+
+#This is a comment
+#The next line is wrong
+#p .5 / 2
+#the 0 goes before the dot
+
+=begin
+This
+is
+a
+comment
+=end
+
+
+#Variable
+a = 5
+b = 4 + 2
+p a + b
+
+ name = "Carl"
+ last_name = " Smith"
+ puts name + last_name
+ b = 2
+ puts a + b
+
