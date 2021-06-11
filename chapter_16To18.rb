24db017f7aa2a11c55642539a73812087f50cd34
GustavoAndresRamirezMunoz <gustavo.ramirez@kommit.co>
2021-06-11 17:08:51 -0500

    chapter 16 to 18

---
+++ chapter_16To18.rb
@@ -0,0 +1,20 @@
+puts "Hello with puts"
+print "Hello with print"
+puts
+p "Hello with p"
+
+puts 5
+print 5
+puts
+p 5
+
+puts "5"+"4"
+print "5"+"4"
+puts
+p "5"+"4"
+
+
+puts "this is
+a line break"
+p "this is
+a line break literally"
