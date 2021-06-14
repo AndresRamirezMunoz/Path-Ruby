0ed606abb20d265ad8971dedbc1dff6e6baf3e17
GustavoAndresRamirezMunoz <gustavo.ramirez@kommit.co>
2021-06-14 10:53:39 -0500

    chapter 28 to 30

---
+++ chapter_28to30.rb
@@ -0,0 +1,26 @@
+#class of numbers
+p 5.class
+p 2.5.class
+
+#Convert
+str = "15"
+p str.to_i
+p str.to_f
+
+number = 20
+p number.to_s
+p number.to_f
+
+number = 20.5555
+p number.to_s
+p number.to_i
+
+
+#booleans
+a = true
+b = false
+p a
+p b
+
+p a.class
+p b.class
