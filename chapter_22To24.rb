63e81ddb93a67facad36730cce44a727e4a2b320
GustavoAndresRamirezMunoz <gustavo.ramirez@kommit.co>
2021-06-14 08:33:21 -0500

    chapter 22 to 24

---
+++ chapter_22To24.rb
@@ -0,0 +1,21 @@
+#Parallel Variable Assignment and Swapping Variable Values
+a, b, c = 10, 20, 30
+p a, b, c
+
+a, b = b, a
+p a, b, c
+
+#Constants
+NAME = "Lois"
+PI = 3.15159
+
+p "Her name is #{NAME}"
+p PI
+
+#Warning
+NAME ="Charles"
+
+#Object methods
+p NAME.length
+puts NAME.inspect
+p NAME
