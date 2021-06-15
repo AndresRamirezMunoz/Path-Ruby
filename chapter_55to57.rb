2698025188d18c34bbd3af0c91782bfd61d61c6f
GustavoAndresRamirezMunoz <gustavo.ramirez@kommit.co>
2021-06-15 08:27:30 -0500

    Chapters 55 to 57

---
+++ chapter_55to57.rb
@@ -0,0 +1,21 @@
+#Bang methods
+word = "hello"
+word.capitalize!
+p word
+
+word.upcase!
+p word
+
+word.swapcase!
+p word
+puts
+
+#The include? method
+name ="Jhon Snow"
+p name.include?("wa")
+p name.upcase.include?("JHON")
+puts
+
+#the empty? and nil? methods
+p "content".empty?
+p "".nil?
